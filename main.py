3a5277c877e4de3251e8c910c5342a999be4f3e1
AjsonZ <51015437+AjsonZ@users.noreply.github.com>
2019-08-29 12:52:48 -0400

    completes all

--- main.py
+++ main.py
@@ -5,4 +5,9 @@ import utils
 utils.check_version((3,7))
 utils.clear()
 
-print('Hello, Class!')
+print('My name is Jason Zhang')
+print('My favorite game is overwatch')
+print("I'm concerning about if I can catch up with what teacher just say becasue my English is not good. ")
+print('I am excited about learning new things')
+print('user:11980374')
+print('https://github.com/AjsonZ')
